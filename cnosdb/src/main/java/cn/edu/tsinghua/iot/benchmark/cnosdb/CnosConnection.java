25af84a64a7730e2496dccf16b7ae86fdd30fdb7
liikeuforever <xuzihang829@outlook.com>
2024-01-16 14:23:23 +0800

    update review problems

--- cnosdb/src/main/java/cn/edu/tsinghua/iot/benchmark/cnosdb/CnosConnection.java
+++ cnosdb/src/main/java/cn/edu/tsinghua/iot/benchmark/cnosdb/CnosConnection.java
@@ -15,9 +15,13 @@ import java.util.concurrent.TimeUnit;
 
 public class CnosConnection {
   private final String url;
+<<<<<<< HEAD
 
   private static Config config = ConfigDescriptor.getInstance().getConfig();
 
+=======
+  private static Config config = ConfigDescriptor.getInstance().getConfig();
+>>>>>>> 54033260 (update review problems)
 
   OkHttpClient client;
 
